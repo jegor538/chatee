1853f7753e614cf1bf444175b515ea395c4b86f3
jegor538 <jegor538@proton.me>
2026-03-24 22:15:46 +0100

    Initialize Supabase client with URL and anon key

---
+++ supabase.js
@@ -0,0 +1,4 @@
+const supabaseUrl = 'https://yjoxhngjgsomxpypepes.supabase.co';
+const supabaseAnonKey = 'sb_publishable_z_u6MQT2qdaNJ3ox-qMaxA_zyUFfslx';
+
+const supabase = window.supabase.createClient(supabaseUrl, supabaseAnonKey);
